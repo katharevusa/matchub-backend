2c80e9aff6fbb9830c40d9d852305a2b99f9a67e
longluqiansoc <luqian@comp.nus.edu.sg>
2020-09-13 16:37:44 +0800

    More project related use cases

---
+++ src/main/java/com/is4103/matchub/controller/KPIController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.is4103.matchub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author longluqian
+ */
+
+@RestController
+@RequestMapping("/authenticated")
+public class KPIController {
+    
+    
+    
+    //create KPI (projectId, KPI object)
+    
+    //get a list of KPIs (projectId)
+    
+    
+    //update KPIs (updatorId, KPI id, KPI entity
+    
+    
+    //Delete KPI(deletor id, kpi id)
+    
+    
+    
+    
+}
---
+++ src/main/java/com/is4103/matchub/controller/VolunteerController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.is4103.matchub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author longluqian
+ */
+
+@RestController
+@RequestMapping("/authenticated")
+public class VolunteerController {
+    
+    
+    
+    // get a list of join request(project id)
+    
+    
+    //decide on a request (requestid, boolean decision)
+    
+    
+    //view a list of teammembers (project id)
+    
+    
+    //
+}
