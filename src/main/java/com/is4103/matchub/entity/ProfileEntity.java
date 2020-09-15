2ca6ce1ad9f21d2a62a086a23f9d0602a3554bf5
Jun Le <tayjunle@gmail.com>
2020-09-16 03:43:23 +0800

    fix create project and data init

--- src/main/java/com/is4103/matchub/entity/ProfileEntity.java
+++ src/main/java/com/is4103/matchub/entity/ProfileEntity.java
@@ -75,6 +75,7 @@ public abstract class ProfileEntity extends AccountEntity {
     private List<ResourceEntity> hostedResources = new ArrayList<>();
 
     @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"projects"})
     private List<SDGEntity> sdgs = new ArrayList<>();
 
     @ManyToMany(fetch = FetchType.LAZY)
