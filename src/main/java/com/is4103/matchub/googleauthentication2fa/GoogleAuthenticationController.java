c374e2a3574b5018486c6f1f1b2ecaa0bfbc18c5
jinggggwenn <e0323466@u.nus.edu>
2020-09-16 23:57:58 +0800

    created 2 methods - generateQRCodeImage & generateSecret

--- src/main/java/com/is4103/matchub/googleauthentication2fa/GoogleAuthenticationController.java
+++ src/main/java/com/is4103/matchub/googleauthentication2fa/GoogleAuthenticationController.java
@@ -13,14 +13,18 @@ import com.is4103.matchub.service.UserService;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 /**
  *
@@ -39,29 +43,63 @@ public class GoogleAuthenticationController {
     private UserService userService;
 
     @SneakyThrows
-    @GetMapping("/generateQRCode/{email}")
-    public void generateQRCode(@PathVariable String email, HttpServletResponse response) {
+    @RequestMapping(method = RequestMethod.GET, value = "/generateQRCodeImage/{email}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] generateQRCodeImage(@PathVariable String email, HttpServletResponse response) throws Exception {
         final GoogleAuthenticatorKey key = gAuth.createCredentials(email);
 
-        // generate QRCode on backend site
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-
         String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("matchub", email, key);
-        System.out.println(otpAuthURL);
 
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(otpAuthURL, BarcodeFormat.QR_CODE, 200, 200);
 
-        //Simple writing to outputstream 
-        ServletOutputStream outputStream = response.getOutputStream();
-        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
-        outputStream.close();
+        //convert buffered image into btye array 
+        BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", baos);
+        baos.flush();
+        byte[] imageInByte = baos.toByteArray();
+        baos.close();
+        return imageInByte;
+
     }
 
-    @PostMapping("/validate2FAKey")
+    @SneakyThrows
+    @RequestMapping(method = RequestMethod.GET, value = "/generateSecret/{email}")
+    public String generateSecret(@PathVariable String email, HttpServletResponse response) throws Exception {
+        final GoogleAuthenticatorKey key = gAuth.createCredentials(email);
+
+        String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("matchub", email, key);
+        int startIndex = 7 + otpAuthURL.lastIndexOf("secret=");
+        int endIndex = otpAuthURL.indexOf("&");
+        String secret = otpAuthURL.substring(startIndex, endIndex);
+
+        System.out.println(secret);
+        return secret;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/validate2FAKey")
     public Boolean validateKey(@RequestBody Validate2FAVO body) {
         Boolean valid2fa = gAuth.authorizeUser(body.getEmail(), body.getCode());
         userService.set2FAValidity(body.getEmail(), valid2fa);
         return valid2fa;
-
     }
+    
+    //    @SneakyThrows
+//    @GetMapping("/generateQRCode/{email}")
+//    public void generateQRCode(@PathVariable String email, HttpServletResponse response) {
+//        final GoogleAuthenticatorKey key = gAuth.createCredentials(email);
+//
+//        // generate QRCode on backend site
+//        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+//
+//        String otpAuthURL = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("matchub", email, key);
+//        System.out.println(otpAuthURL);
+//
+//        BitMatrix bitMatrix = qrCodeWriter.encode(otpAuthURL, BarcodeFormat.QR_CODE, 200, 200);
+//
+//        //Simple writing to outputstream 
+//        ServletOutputStream outputStream = response.getOutputStream();
+//        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+//        outputStream.close();
+//    }
 }
