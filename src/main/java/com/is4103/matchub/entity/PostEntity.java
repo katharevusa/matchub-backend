6b97d6562dcb7bcce6aed097c26ec8c6b22f333a
jinggggwenn <e0323466@u.nus.edu>
2020-09-23 01:41:02 +0800

    added length=1000 for content in PostEntity

--- src/main/java/com/is4103/matchub/entity/PostEntity.java
+++ src/main/java/com/is4103/matchub/entity/PostEntity.java
@@ -41,7 +41,7 @@ public class PostEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
     
-    @Column(nullable= false)
+    @Column(nullable= false, length = 1000)
     @NotNull
     private String content;
     
