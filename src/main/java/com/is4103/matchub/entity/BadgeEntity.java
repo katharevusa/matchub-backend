d1ea121d61f6a542c7fdeafcf7128df91cba7997
jinggggwenn <e0323466@u.nus.edu>
2020-09-27 00:46:01 +0800

    added JsonIgnoreProperties in BadgeEntity

--- src/main/java/com/is4103/matchub/entity/BadgeEntity.java
+++ src/main/java/com/is4103/matchub/entity/BadgeEntity.java
@@ -5,6 +5,7 @@
  */
 package com.is4103.matchub.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.is4103.matchub.enumeration.BadgeTypeEnum;
 import java.util.ArrayList;
 import java.util.List;
@@ -53,9 +54,11 @@ public class BadgeEntity {
     private String icon;
 
     @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"posts", "hostedResources", "sdgs", "projectsJoined", "projectsOwned", "reviewsReceived", "badges", "likedPosts"})
     private List<ProfileEntity> profiles = new ArrayList<>();
 
     @OneToOne(optional = true)
+    @JsonIgnoreProperties({"projectBadge", "sdgs", "teamMembers"})
     private ProjectEntity project;
 
     public BadgeEntity(BadgeTypeEnum badgeType, String badgeTitle, String icon) {
