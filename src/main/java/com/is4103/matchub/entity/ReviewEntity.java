88c33660e7b96586607bbccbb3608b78f0a2a50b
jinggggwenn <e0323466@u.nus.edu>
2020-09-26 21:57:39 +0800

    added JsonIgnoreProperties on ProjectEntity and ReviewEntity

--- src/main/java/com/is4103/matchub/entity/ReviewEntity.java
+++ src/main/java/com/is4103/matchub/entity/ReviewEntity.java
@@ -5,6 +5,7 @@
  */
 package com.is4103.matchub.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -49,10 +50,12 @@ public class ReviewEntity {
 
     @ManyToOne(optional = false)
     @JoinColumn(nullable = false)
+    @JsonIgnoreProperties({"reviews", "projectOwners", "teamMembers", "listOfRequests"})
     private ProjectEntity project;
 
     @ManyToOne(optional = false)
     @JoinColumn(nullable = false)
+    @JsonIgnoreProperties({"reviewsReceived", "projectsOwned", "hostedResources", "sdgs", "likedPosts"})
     private ProfileEntity reviewReceiver;
 
     public ReviewEntity(String content, BigDecimal rating) {
