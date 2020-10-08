c5107265c5ebd5d3d0cc0c1975bcb083f5805d09
jinggggwenn <e0323466@u.nus.edu>
2020-10-08 12:17:23 +0800

    badge create

--- src/main/java/com/is4103/matchub/vo/ProjectBadgeCreateVO.java
+++ src/main/java/com/is4103/matchub/vo/ProjectBadgeCreateVO.java
@@ -16,21 +16,23 @@ import lombok.Data;
  */
 @Data
 public class ProjectBadgeCreateVO {
-    
+
     @NotNull(message = "Project Id can not be null.")
     private Long projectId;
-    
+
     @NotNull(message = "Badge Title can not be null.")
     @NotBlank(message = "Badge Title can not be blank.")
     private String badgeTitle;
-    
-    @NotNull(message = "Badge Icon can not be null.")
-    @NotBlank(message = "Badge Icon can not be blank.")
+
     private String icon;
-    
+
     public void updateProjectBadge(BadgeEntity badge) {
         badge.setBadgeTitle(this.badgeTitle);
-        badge.setIcon(this.icon);
+
+        if (!icon.isEmpty()) {
+            badge.setIcon(this.icon);
+        }
+
         //associate project with badge in service class
     }
 }
