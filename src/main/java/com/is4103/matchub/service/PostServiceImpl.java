d098a349f48a6db9ced031ce9765b7bbe4b4809e
longluqiansoc <luqian@comp.nus.edu.sg>
2020-11-11 00:26:27 +0800

    debug like post

--- src/main/java/com/is4103/matchub/service/PostServiceImpl.java
+++ src/main/java/com/is4103/matchub/service/PostServiceImpl.java
@@ -305,7 +305,7 @@ public class PostServiceImpl implements PostService {
             throw new LikePostException("You have already liked this post");
         }
         Long oldLikes = post.getLikes();
-        post.setLikes(oldLikes++);
+        post.setLikes(oldLikes+1);
         post.getLikedUsersId().add(likerId);
         post = postEntityRepository.saveAndFlush(post);
 
