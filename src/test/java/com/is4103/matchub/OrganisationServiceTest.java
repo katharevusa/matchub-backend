d0357799c53d0ec7cea4b5065c0b3032ff0015ac
jinggggwenn <e0323466@u.nus.edu>
2020-11-28 14:30:47 +0800

    tested 4 methods of organisationService

--- src/test/java/com/is4103/matchub/OrganisationServiceTest.java
+++ src/test/java/com/is4103/matchub/OrganisationServiceTest.java
@@ -6,8 +6,11 @@
 package com.is4103.matchub;
 
 import com.is4103.matchub.entity.OrganisationEntity;
+import com.is4103.matchub.entity.ProfileEntity;
 import com.is4103.matchub.exception.OrganisationNotFoundException;
+import com.is4103.matchub.exception.UnableToAddKAHToOrganisationException;
 import com.is4103.matchub.exception.UnableToAddMemberToOrganisationException;
+import com.is4103.matchub.exception.UnableToRemoveMemberFromOrganisationException;
 import com.is4103.matchub.exception.UserNotFoundException;
 import com.is4103.matchub.repository.OrganisationEntityRepository;
 import com.is4103.matchub.service.OrganisationService;
@@ -21,6 +24,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Assert;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 /**
  *
@@ -28,6 +37,7 @@ import org.junit.Assert;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 public class OrganisationServiceTest {
 
     public OrganisationServiceTest() {
@@ -55,24 +65,10 @@ public class OrganisationServiceTest {
     @Autowired
     private OrganisationEntityRepository organisationEntityRepository;
 
-    @Test(expected = OrganisationNotFoundException.class)
-    public void addMemberToOrganisationTest1() {
-        organisationService.addMemberToOrganisation(20L, 4L);
-    }
-
-    @Test(expected = UserNotFoundException.class)
-    public void addMemberToOrganisationTest2() {
-        organisationService.addMemberToOrganisation(7L, 20L);
-    }
-
-    @Test(expected = UnableToAddMemberToOrganisationException.class)
-    public void addMemberToOrganisationTest3() {
-        organisationService.addMemberToOrganisation(7L, 6L);
-    }
-
     @Test
-    public void addMemberToOrganisationTest4() {
-        
+    @Order(4)
+    public void testAddMemberToOrganisation() {
+
         organisationService.addMemberToOrganisation(7L, 4L);
 
         try {
@@ -83,9 +79,108 @@ public class OrganisationServiceTest {
         }
 
     }
-    
-    public void removeMemberFromOrganisation1() {
+
+    @Test(expected = OrganisationNotFoundException.class)
+    @Order(1)
+    public void testAddMemberToOrganisationOrganisationNotFoundException() {
+        organisationService.addMemberToOrganisation(20L, 4L);
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    @Order(2)
+    public void testAddMemberToOrganisationUserNotFoundException() {
+        organisationService.addMemberToOrganisation(7L, 20L);
+    }
+
+    @Test(expected = UnableToAddMemberToOrganisationException.class)
+    @Order(3)
+    public void testAddMemberToOrganisationUnableToAddMemberToOrganisationException() {
+        organisationService.addMemberToOrganisation(7L, 6L);
+    }
+
+    @Test
+    @Order(8)
+    public void testRemoveMemberFromOrganisation() {
+
+        organisationService.removeMemberFromOrganisation(7L, 12L);
+
+        try {
+            OrganisationEntity organisation = organisationEntityRepository.findById(7L).get();
+            Assert.assertTrue(!organisation.getEmployees().contains(9L));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test(expected = OrganisationNotFoundException.class)
+    @Order(5)
+    public void testRemoveMemberFromOrganisationOrganisationNotFoundException() {
+        organisationService.removeMemberFromOrganisation(20L, 4L);
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    @Order(6)
+    public void testRemoveMemberFromOrganisationUserNotFoundException() {
+        organisationService.removeMemberFromOrganisation(7L, 20L);
+    }
+
+    @Test(expected = UnableToRemoveMemberFromOrganisationException.class)
+    @Order(7)
+    public void testRemoveMemberFromOrganisationUnableToRemoveMemberFromOrganisationException() {
+        organisationService.removeMemberFromOrganisation(7L, 9L);
+    }
+
+    @Test
+    @Order(10)
+    public void testViewOrganisationMembers() {
+        Pageable pageable = PageRequest.of(0, 20);
+        Page<ProfileEntity> results = organisationService.viewOrganisationMembers(8L, pageable);
+
+        Assert.assertTrue(results.getContent().size() == 2);
+    }
+
+    @Test(expected = OrganisationNotFoundException.class)
+    @Order(9)
+    public void testViewOrganisationMembersOrganisationNotFoundException() {
+        Pageable pageable = PageRequest.of(0, 20);
+        organisationService.viewOrganisationMembers(20L, pageable);
+    }
+
+    @Test
+    @Order(14)
+    public void testAddKahToOrganisation() {
+        organisationService.addMemberToOrganisation(7L, 11L);
+        organisationService.addKahToOrganisation(7L, 11L);
         
+        try {
+            OrganisationEntity organisation = organisationEntityRepository.findById(7L).get();
+            Assert.assertTrue(!organisation.getKahs().contains(12L));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test(expected = OrganisationNotFoundException.class)
+    @Order(11)
+    public void testAddKahToOrganisationOrganisationNotFoundException() {
+        organisationService.addKahToOrganisation(20L, 4L);
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    @Order(12)
+    public void testAddKahToOrganisationUserNotFoundException() {
+        organisationService.addKahToOrganisation(7L, 20L);
+    }
+
+    @Test(expected = UnableToAddKAHToOrganisationException.class)
+    @Order(13)
+    public void testAddKahToOrganisationUnableToAddKAHToOrganisationException() {
+        organisationService.addKahToOrganisation(7L, 6L);
+    }
+    
+    @Test
+    public void testRemoveKahFromOrganisation() {
+        organisationService.removeKahFromOrganisation(7L, 6L);
     }
 
 }
