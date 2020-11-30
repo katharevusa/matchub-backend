ace2888dabbc8acac067c2f3f16485bd92d41b11
Jun Le <tayjunle@gmail.com>
2020-11-30 23:51:05 +0800

    amend user statistics month logic

--- src/main/java/com/is4103/matchub/service/SystemAdminServiceImpl.java
+++ src/main/java/com/is4103/matchub/service/SystemAdminServiceImpl.java
@@ -12,7 +12,9 @@ import com.is4103.matchub.repository.ProfileEntityRepository;
 import com.is4103.matchub.repository.ProjectEntityRepository;
 import com.is4103.matchub.repository.ResourceEntityRepository;
 import com.is4103.matchub.repository.ResourceTransactionEntityRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
@@ -68,68 +70,51 @@ public class SystemAdminServiceImpl implements SystemAdminService {
     public Map<String, Integer> getLastFiveUserNumberData() {
 
         Map<String, Integer> map = new HashMap<>();
-        LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println("currentTime" + currentTime);
-        LocalDateTime lastOneMonthTime = currentTime.minus(1, ChronoUnit.MONTHS);
-        LocalDateTime lastTwoMonthTime = currentTime.minus(2, ChronoUnit.MONTHS);
-        LocalDateTime lastThreeMonthTime = currentTime.minus(3, ChronoUnit.MONTHS);
-        LocalDateTime lastFourMonthTime = currentTime.minus(4, ChronoUnit.MONTHS);
-        LocalDateTime lastFiveMonthTime = currentTime.minus(5, ChronoUnit.MONTHS);
-
-        int lastMonth = profileEntityRepository.findUsersByJoinDate(lastOneMonthTime, currentTime).size();
-        int secondLastMonth = profileEntityRepository.findUsersByJoinDate(lastTwoMonthTime, lastOneMonthTime).size();
-        int ThirdLastMonth = profileEntityRepository.findUsersByJoinDate(lastThreeMonthTime, lastTwoMonthTime).size();
-        int FourthLastMonth = profileEntityRepository.findUsersByJoinDate(lastFourMonthTime, lastThreeMonthTime).size();
-        int FifthLastMonth = profileEntityRepository.findUsersByJoinDate(lastFiveMonthTime, lastFourMonthTime).size();
-
-    
-        map.put("last month data", lastMonth);
-        map.put("last second month data", secondLastMonth);
-        map.put("last third month data", ThirdLastMonth);
-        map.put("last fourth month data", FourthLastMonth);
-        map.put("last fifth month data", FifthLastMonth);
-        System.out.println(map.toString());
+        
+        for (int i = 0; i < 6; i++) {
+            LocalDateTime startMonth = YearMonth.now().atDay(1).minus(i, ChronoUnit.MONTHS).atStartOfDay();
+            LocalDateTime startOfNextMonth = startMonth.plus(1, ChronoUnit.MONTHS);
+            
+            // make into capital case
+            map.put(startMonth.getMonth().toString().substring(0, 1) + startMonth.getMonth().toString().substring(1).toLowerCase(), profileEntityRepository.findUsersByJoinDate(startMonth, startOfNextMonth).size());
+        }
 
         return map;
     }
 
 //Monthly transactions meaning last 5 month monthly total transactions is it
-
     @Override
     public Map<String, Integer> getLastFiveTransactionNumberData() {
         Map<String, Integer> map = new HashMap<>();
         LocalDateTime currentTime = LocalDateTime.now();
-   
+
         LocalDateTime lastOneMonthTime = currentTime.minus(1, ChronoUnit.MONTHS);
         LocalDateTime lastTwoMonthTime = currentTime.minus(2, ChronoUnit.MONTHS);
         LocalDateTime lastThreeMonthTime = currentTime.minus(3, ChronoUnit.MONTHS);
         LocalDateTime lastFourMonthTime = currentTime.minus(4, ChronoUnit.MONTHS);
         LocalDateTime lastFiveMonthTime = currentTime.minus(5, ChronoUnit.MONTHS);
 
-        int lastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastOneMonthTime, currentTime).size()+
-                donationEntityRepository.findDonationsByTransactionTime(lastOneMonthTime, currentTime).size();
-        int secondLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastTwoMonthTime, lastOneMonthTime).size()+
-                donationEntityRepository.findDonationsByTransactionTime(lastTwoMonthTime, lastOneMonthTime).size();
-        int ThirdLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastThreeMonthTime, lastTwoMonthTime).size()+
-                donationEntityRepository.findDonationsByTransactionTime(lastThreeMonthTime, lastTwoMonthTime).size();
-        int FourthLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastFourMonthTime, lastThreeMonthTime).size()+
-                donationEntityRepository.findDonationsByTransactionTime(lastFourMonthTime, lastThreeMonthTime).size();
-        int FifthLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastFiveMonthTime, lastFourMonthTime).size()+
-                donationEntityRepository.findDonationsByTransactionTime(lastFiveMonthTime, lastFourMonthTime).size();
+        int lastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastOneMonthTime, currentTime).size()
+                + donationEntityRepository.findDonationsByTransactionTime(lastOneMonthTime, currentTime).size();
+        int secondLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastTwoMonthTime, lastOneMonthTime).size()
+                + donationEntityRepository.findDonationsByTransactionTime(lastTwoMonthTime, lastOneMonthTime).size();
+        int ThirdLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastThreeMonthTime, lastTwoMonthTime).size()
+                + donationEntityRepository.findDonationsByTransactionTime(lastThreeMonthTime, lastTwoMonthTime).size();
+        int FourthLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastFourMonthTime, lastThreeMonthTime).size()
+                + donationEntityRepository.findDonationsByTransactionTime(lastFourMonthTime, lastThreeMonthTime).size();
+        int FifthLastMonth = resourceTransactionEntityRepository.findResourceTransactionEntityByTransactionTime(lastFiveMonthTime, lastFourMonthTime).size()
+                + donationEntityRepository.findDonationsByTransactionTime(lastFiveMonthTime, lastFourMonthTime).size();
 
         map.put("last month data", lastMonth);
         map.put("last second month data", secondLastMonth);
         map.put("last third month data", ThirdLastMonth);
         map.put("last fourth month data", FourthLastMonth);
         map.put("last fifth month data", FifthLastMonth);
-        
-        
+
         return map;
     }
 
-
-    
-   @Override
+    @Override
     public Map<String, Integer> getProjectsNumberWithDifferentStatus() {
         Map<String, Integer> map = new HashMap<>();
         map.put(String.valueOf(ProjectStatusEnum.ACTIVE), projectEntityRepository.getProjectsByStatus(ProjectStatusEnum.ACTIVE).size());
